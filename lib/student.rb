cc64a32c0e47e5bba2924683dabf0909d592c4e5
Ayana Bando <ayanaflax@yahoo.com>
2018-09-07 14:52:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,12 @@ end
   end
   
   def self.new_from_db(row)
-    binding.pry
+    #binding.pry
+        new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
 end
